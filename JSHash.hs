866a4128cc59c0720a9d3662187271364e4ca127
Bastiaan Zapf <bzapf@online.de>
2014-09-12 19:22:03 +0200

    verträgliche Datentypen

--- JSHash.hs
+++ JSHash.hs
@@ -16,28 +16,28 @@ class Hashable a where
 instance Show x => Hashable x where
     hashKey = show
 
-foreign import ccall jsNewHash :: JSString -> IO ()
-foreign import ccall jsStoreHash :: JSString -> JSString -> Ptr a -> IO ()
-foreign import ccall jsReadHash :: JSString -> JSString -> IO (Ptr a)
-foreign import ccall jsPush :: JSString -> Ptr a -> IO ()
-foreign import ccall jsPop :: JSString -> IO (Ptr a)
+foreign import ccall jsNewHash :: Ptr JSString -> IO ()
+foreign import ccall jsStoreHash :: Ptr JSString -> Ptr JSString -> Ptr a -> IO ()
+foreign import ccall jsReadHash :: Ptr JSString -> Ptr JSString -> IO (Ptr a)
+foreign import ccall jsPush :: Ptr JSString -> Ptr a -> IO ()
+foreign import ccall jsPop :: Ptr JSString -> IO (Ptr a)
 
 newHash :: String -> Client (JSHash key a)
-newHash str = do liftIO $ jsNewHash $ toJSStr str
+newHash str = do liftIO $ jsNewHash $ toPtr $ toJSStr str
                  return $ Mknt str
 
 
 storeHash :: (Hashable key,Show a) => (JSHash key a) -> key -> a -> Client ()
 storeHash (Mknt hash) key value = liftIO $ 
-    jsStoreHash (toJSStr hash) (toJSStr $ hashKey key) (toPtr value)
+    jsStoreHash (toPtr $ toJSStr hash) (toPtr $ toJSStr $ hashKey key) (toPtr value)
 
 readHash :: (Hashable key, Unpack a,Pack a, Read a,Show a ) => (JSHash key a) -> key -> Client a
-readHash (Mknt hash) key = do x <- liftIO $ jsReadHash (toJSStr hash) (toJSStr $ hashKey key)
+readHash (Mknt hash) key = do x <- liftIO $ jsReadHash (toPtr $ toJSStr hash) (toPtr $ toJSStr $ hashKey key)
                               return $ fromPtr x
 
 push :: (Show b) => JSHash Int b -> b -> Client () 
-push (Mknt hash) value = liftIO $ jsPush (toJSStr hash) (toPtr value)
+push (Mknt hash) value = liftIO $ jsPush (toPtr $ toJSStr hash) (toPtr value)
 
 pop :: (Pack b, Unpack b) => JSHash Int b -> Client b 
-pop (Mknt hash) = do x <- liftIO $ jsPop (toJSStr hash)
+pop (Mknt hash) = do x <- liftIO $ jsPop (toPtr $ toJSStr hash)
                      return $ fromPtr x
