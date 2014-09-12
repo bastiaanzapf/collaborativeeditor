4ad15e8f19c3779520c022fe4c0c2a8120e61a37
Bastiaan Zapf <bzapf@online.de>
2014-09-12 16:46:21 +0200

    mergeIntoHash kompiliert

--- editor.hs
+++ editor.hs
@@ -11,17 +11,17 @@ import JSHash
 
 --newtype Id = Mk_Id (Int,Int) deriving (Eq, Show, Read)
 
-data Id = Mk_Id (Int,Int) deriving (Show, Read, Eq)
+data Id = Mk_Id (Int,Int) deriving (Show, Read, Eq, Ord)
 
 id_Begin = Mk_Id (0,0)
-id_End   = Mk_Id (0,1)
+id_End   = Mk_Id (9999999,0)
 
 data W_Character = W_Character { id          :: Id
                                , visible     :: Bool
                                , literal     :: Char
                                , previous_id :: Id
                                , next_id     :: Id    } 
-                   deriving (Show, Read)
+                   deriving (Show, Read, Eq)
 
 instance Pack W_Character where
 
@@ -59,6 +59,48 @@ wc1 = W_Character {Main.id=Mk_Id (1,1),visible=True,literal='a',previous_id=id_B
 wc2 = W_Character {Main.id=Mk_Id (1,2),visible=True,literal='b',previous_id=id_Begin,next_id=id_End}
 
 wc3 = W_Character {Main.id=Mk_Id (1,4),visible=True,literal='c',previous_id=id_Begin,next_id=id_End}
+
+insert hash previous_id next_id wchar =
+    do previous <- readHash hash $ previous_id wchar
+       next     <- readHash hash $ next_id     wchar
+
+       let storeWithId x = storeHash hash (Main.id x) x
+
+       storeWithId $ W_Character {Main.id=Main.id previous,
+                                  visible=visible previous,
+                                  literal=literal previous,
+                                  previous_id=previous_id previous,
+                                  next_id=Main.id wchar}
+
+       storeWithId wchar
+
+       storeWithId $ W_Character {Main.id=Main.id next,
+                                  visible=visible next,
+                                  literal=literal next,
+                                  previous_id=Main.id wchar,
+                                  next_id=next_id next}
+
+findPosition (hwc:hwc2:twc) wc = if (Main.id wc)>=(Main.id hwc)
+                                    then (hwc,hwc2)
+                                    else findPosition (hwc2:twc) wc
+
+findPosition (hwc:[]) _ = error "Could not find position"
+findPosition [] _ = error "Could not find position"
+
+between wc1 wc2 wc = W_Character {Main.id=Main.id wc,
+                                  visible=visible wc,
+                                  literal=literal wc,
+                                  previous_id=Main.id wc1,
+                                  next_id=Main.id wc2}
+
+mergeIntoHash hash wchar = 
+    do seq <- subseq hash (previous_id wchar) (next_id wchar)
+       if seq == []
+       then insert hash previous_id next_id wchar
+       else do next <- readHash hash (next_id wchar)               
+               let inclseq = seq ++ [ next ]
+               let (a,b) = findPosition inclseq wchar
+               mergeIntoHash hash (between a b wchar)
 
 clientMain :: IO ()
 clientMain = withElems ["editor"] $ 
