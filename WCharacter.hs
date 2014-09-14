cf4f057d64e3e90b8de8b4ea643bec906cb893a0
Bastiaan Zapf <bzapf@online.de>
2014-09-14 11:46:35 +0200

    Id wird geschrieben und gelesen

--- WCharacter.hs
+++ WCharacter.hs
@@ -10,7 +10,9 @@ data Id = Mk_Id (Int,Int) deriving (Show, Read, Eq, Ord)
 
 instance Binary Id where
     put (Mk_Id (a,b)) = put a >> put b
-    get = get
+    get = do a <- get
+             b <- get
+             return $ Mk_Id (a,b)
 
 data W_Character = W_Character { id          :: WCharacter.Id
                                , visible     :: Bool
@@ -25,15 +27,17 @@ instance Unpack W_Character where
 
 instance Binary W_Character where
     put w_char = do putWord8 0
+                    put $ WCharacter.id w_char
                     putWord8 $ fromIntegral $ ord $ literal w_char
                     if (visible w_char) then putWord8 1
                                         else putWord8 0
     get = do x <- getWord8
-             chr <- getWord8
+             id' <- get
+             lit <- getWord8
              visible <- getWord8
-             return $ W_Character {WCharacter.id=Mk_Id (-18,2),
+             return $ W_Character {WCharacter.id=id',
                                    visible=True,
-                                   literal=toEnum $ fromEnum chr,
+                                   literal=toEnum $ fromEnum lit,
                                    next_id=Mk_Id (-19,0),
                                    previous_id=Mk_Id (-20,0)
                                   }
